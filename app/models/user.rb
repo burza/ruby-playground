7ed444ac91e1b33413012bd69b6ee72cd0aa25f8
Gregor Swatek <gregor.swatek@mindmatters.de>
2010-11-05 13:09:04 +0100

    added dynamic instance variables, added dynamic attr accessors for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,33 @@
 class User
-  attr_accessor :name
 
-  def initialize(name)
-    @name = name
+  #attr_accessor :name
+
+  def initialize(params = {})
+    params.each_pair {|key, value|
+      self.instance_variable_set("@#{key}", value)
+      self.instance_eval %{
+        def #{key}
+          @#{key}
+        end
+        def #{key}=(value)
+          @#{key} = value
+        end
+      }
+    }
   end
 
   def say_hello
     puts "Hallo, #{@name}"
   end
-  
-  eval("#{variable_name} = #[value}")
-  for instance variables:
-  self.instance_variable_set(:@var_name, value)
+
+  def say_byby
+    puts "Cu l8er, #{@name}!"
+  end
+
+  def render_template(user)
+    # open a template file
+    # parse the file for keywords
+    # render the template with keywords
+  end
+
 end
